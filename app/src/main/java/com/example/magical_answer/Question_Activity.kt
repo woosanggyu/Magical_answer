2f7a1a7cd8392e4a1d7289219176a385ca61d1ab
WooSangGyu <wsg0701@naver.com>
2020-08-07 14:06:42 +0900

    Server address and port change

--- app/src/main/java/com/example/magical_answer/Question_Activity.kt
+++ app/src/main/java/com/example/magical_answer/Question_Activity.kt
@@ -185,7 +185,7 @@ class Question_Activity : AppCompatActivity() {
 
     //레트로핏 객체 선언
     val retrofit = Retrofit.Builder()
-        .baseUrl("http://ec2-15-164-250-192.ap-northeast-2.compute.amazonaws.com:3000") //서버주소
+        .baseUrl("http://ec2-54-180-91-26.ap-northeast-2.compute.amazonaws.com:8080") //서버주소
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
