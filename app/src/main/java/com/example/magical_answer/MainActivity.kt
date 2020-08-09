050df40f051fdde43e8bea8015545f844a5cbffc
WooSangGyu <wsg0701@naver.com>
2020-08-09 19:25:40 +0900

    Password inputType change, MemoActivity authority check

--- app/src/main/java/com/example/magical_answer/MainActivity.kt
+++ app/src/main/java/com/example/magical_answer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,8 +19,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         memo_btn.setOnClickListener {
-            val intent = Intent(this@MainActivity, Memo_Activity::class.java)
-            startActivity(intent)
+            if(intent.hasExtra("token")){
+                val intent = Intent(this@MainActivity, Memo_Activity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "로그인되지 않았습니다.", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         Login_btn.setOnClickListener {
